97888d59bb49292ffa2a7cb84452cf7059a9d0f8
Patrick Bender <36022710+Patrick-Bender@users.noreply.github.com>
2021-03-05 10:52:34 -0800

    Update settings.py

--- flaskapp-Backup/flaskapp/settings.py
+++ flaskapp-Backup/flaskapp/settings.py
@@ -1,4 +1,4 @@
 import os
 class Config(object):
     MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/'
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'NBc3zJFuwAbfMcRiXsm3dFZFmsGe8y'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or #insert secret key here
